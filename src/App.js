869d4d6a9c2775fed1219af4898f61a3c836b553
Gnatienko <gnatienko@gmail.com>
2022-11-26 22:53:26 +0200

    DB update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import { useNavigate } from "react-router-dom"
 import { Menu } from "antd"
+import Roles from "./pages/Roles"
+import Executors from "./pages/Executors"
 
-import Roles from './pages/Roles'
-import Executors from './pages/Executors'
-
+const SubMenu = Menu.SubMenu;
 
 function App() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   return (
     <div style={{ display: "flex", msFlexDirection: "row" }}>
       <Menu
+        mode="inline"
+        style={{ width: "10%" }}
         onClick={({ key }) => {
-          navigate(key);
+          navigate(key)
         }}
-        items={[
-          { label: "Roles", key: "/roles" },
-          { label: "Executors", key: "/executors" },
-        ]}
-      ></Menu>
-      <Roles/>
-      <Executors/>
+      >
+        <SubMenu title={<span>Roles</span>}>
+        <Menu.Item key="/roles/create">Create role</Menu.Item>
+        <Menu.Item key="/roles">All roles</Menu.Item>
+        </SubMenu>
+        
+        <SubMenu title={<span>Executors</span>}>
+        <Menu.Item key="/executors">All executors</Menu.Item>
+        </SubMenu>
+
+      </Menu>
+      <Roles />
+      <Executors />
     </div>
-  );
+  )
 }
 
 export default App
