715e3e129fb9bef070d0cf72bfa74176625d7643
Gnatienko <gnatienko@gmail.com>
2022-12-11 18:55:26 +0200

    /role

--- src/pages/Role.js
+++ src/pages/Role.js
@@ -1,4 +1,4 @@
-import { Route, Routes, useParams } from "react-router-dom"
+import { Route, Routes, useParams, useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { Button, Form, Input, Slider } from "antd"
 
@@ -6,6 +6,9 @@ function Element() {
   let { id } = useParams()
 
   const [role, setRole] = useState([])
+
+  const navigate = useNavigate()
+
   useEffect(() => {
     const getRole = (id) => {
       fetch("https://d14f98cedwjzih.cloudfront.net/role/?id=" + id)
@@ -17,21 +20,47 @@ function Element() {
     getRole(id)
   }, [id])
 
+  const handleChange = (field, newVal) => {
+    setRole({
+      ...role,
+      [field]: newVal,
+    })
+  }
+
+  const Submit = () => {
+    const options = { method: "PUT" }
+    fetch(
+      "https://d14f98cedwjzih.cloudfront.net/role?name=" +
+        role.name +
+        "&weight=" +
+        role.weight +
+        "&id=" +
+        role.id,
+      options
+    )
+    navigate("/roles")
+  }
+
   return (
     <Form>
       <Form.Item label="Name">
-        <Input value={role.name}></Input>
+        <Input
+          value={role.name}
+          onChange={(e) => handleChange("name", e.target.value)}
+        ></Input>
       </Form.Item>
       <Form.Item label="Weight">
         <Slider
           min={0}
           max={10}
-          style={{ width: "30rem" }}
           value={role.weight}
+          onChange={(e) => handleChange("weight", e)}
         ></Slider>
       </Form.Item>
       <Form.Item>
-        <Button type="primary">Submit</Button>
+        <Button onClick={Submit} type="primary">
+          Submit
+        </Button>
       </Form.Item>
     </Form>
   )
