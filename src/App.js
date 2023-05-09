10fd2d4aa9b7947a6c0313fe2d12f0c8bfc400f6
Gnatienko <gnatienko@gmail.com>
2023-05-09 15:12:31 +0300

    Added header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useNavigate, Route, Routes } from "react-router-dom"
-import { Menu } from "antd"
+import { Menu, Divider } from "antd"
 import { GoogleLogin } from "@react-oauth/google"
 import Roles from "./pages/Roles"
 import Executors from "./pages/Executors"
@@ -8,8 +8,10 @@ import Role from "./pages/Role"
 import Executor from "./pages/Executor"
 import AddExecutor from "./pages/AddExecutor"
 import Home from "./pages/Home"
-
 import "./App.css"
+
+import React from "react"
+import logo from "./logo.png"
 
 const SubMenu = Menu.SubMenu
 
@@ -17,60 +19,77 @@ function App() {
   const navigate = useNavigate()
 
   return (
-    <div
-      style={{
-        display: "flex",
-        msFlexDirection: "row",
-        "align-items": "top",
-        "justify-content": "space-between",
-      }}
-    >
-      <div style={{ display: "flex", msFlexDirection: "row" }}>
-        <Menu
-          mode="inline"
-          style={{ width: "30rem" }}
-          onClick={({ key }) => {
-            navigate(key)
+    <div>
+      <div
+        style={{
+          margin: "10px",
+          display: "flex",
+          msFlexDirection: "row",
+          "justify-content": "space-between",
+        }}
+      >
+        <div>
+          {" "}
+          <img src={logo} style={{ width: 40 }} alt="icon" /> Resilience
+        </div>
+
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse)
           }}
-        >
-          <Menu.Item key="/">Home</Menu.Item>
-          <SubMenu key="roles" title={<span>Roles</span>}>
-            <Menu.Item key="/roles">All roles</Menu.Item>
-            <Menu.Item key="/roles/add">Add role</Menu.Item>
-          </SubMenu>
-          <SubMenu key="executors" title={<span>Executors</span>}>
-            <Menu.Item key="/executors">All executors</Menu.Item>
-            <Menu.Item key="/executors/add">Add executor</Menu.Item>
-          </SubMenu>
-        </Menu>
-
-        <Routes>
-          <Route path="/role/:id" element={<Role />}></Route>
-          <Route path="/" element={<Home />}></Route>
-
-          <Route path="/roles" key="/roles" element={<Roles />}></Route>
-          <Route path="/roles/add" element={<AddRole />}></Route>
-          <Route
-            path="/executors"
-            key="/executors"
-            element={<Executors />}
-          ></Route>
-          <Route path="/executors/add" element={<AddExecutor />}></Route>
-          <Route
-            path="/executor/:id"
-            key="/executor/:id"
-            element={<Executor />}
-          ></Route>
-        </Routes>
+          onError={() => {
+            console.log("Login Failed")
+          }}
+        />
       </div>
-      <GoogleLogin
-        onSuccess={(credentialResponse) => {
-          console.log(credentialResponse)
+      <Divider style={{ margin: "1px" }} />
+      <div
+        style={{
+          display: "flex",
+          msFlexDirection: "row",
+          "align-items": "top",
+          "justify-content": "space-between",
         }}
-        onError={() => {
-          console.log("Login Failed")
-        }}
-      />
+      >
+        <div style={{ display: "flex", msFlexDirection: "row" }}>
+          <Menu
+            mode="inline"
+            style={{ width: "30rem" }}
+            onClick={({ key }) => {
+              navigate(key)
+            }}
+          >
+            <Menu.Item key="/">Home</Menu.Item>
+            <SubMenu key="roles" title={<span>Roles</span>}>
+              <Menu.Item key="/roles">All roles</Menu.Item>
+              <Menu.Item key="/roles/add">Add role</Menu.Item>
+            </SubMenu>
+            <SubMenu key="executors" title={<span>Executors</span>}>
+              <Menu.Item key="/executors">All executors</Menu.Item>
+              <Menu.Item key="/executors/add">Add executor</Menu.Item>
+            </SubMenu>
+          </Menu>
+
+          <Routes>
+            <Route path="/role/:id" element={<Role />}></Route>
+            <Route path="/" element={<Home />}></Route>
+
+            <Route path="/roles" key="/roles" element={<Roles />}></Route>
+            <Route path="/roles/add" element={<AddRole />}></Route>
+            <Route
+              path="/executors"
+              key="/executors"
+              element={<Executors />}
+            ></Route>
+            <Route path="/executors/add" element={<AddExecutor />}></Route>
+            <Route
+              path="/executor/:id"
+              key="/executor/:id"
+              element={<Executor />}
+            ></Route>
+          </Routes>
+        </div>
+      </div>
     </div>
   )
 }
