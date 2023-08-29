4c40bc368231984f005d133d6f99c59f710aa686
Gnatienko <gnatienko@gmail.com>
2023-08-29 20:49:18 +0300

    - size

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useNavigate, Route, Routes } from "react-router-dom"
-import { Menu, Divider, ConfigProvider, theme, Radio } from "antd"
+import { Menu, Divider } from "antd"
 import { GoogleLogin } from "@react-oauth/google"
 import Roles from "./pages/Roles"
 import Executors from "./pages/Executors"
@@ -9,7 +9,6 @@ import Executor from "./pages/Executor"
 import AddExecutor from "./pages/AddExecutor"
 import Home from "./pages/Home"
 import "./App.css"
-import React, { useState } from "react"
 import logo from "./logo.png"
 
 import { TeamOutlined, SettingOutlined } from "@ant-design/icons"
@@ -18,127 +17,107 @@ const SubMenu = Menu.SubMenu
 
 function App() {
   const navigate = useNavigate()
-  const [componentSize, setComponentSize] = useState("small")
 
   return (
-    <ConfigProvider
-      theme={{
-        algorithm:
-          componentSize === "large"
-            ? theme.defaultAlgorithm
-            : theme.compactAlgorithm,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        "justify-content": "space-between",
+        height: "100vh",
       }}
     >
+      <header
+        style={{
+          margin: "10px",
+          display: "flex",
+          msFlexDirection: "row",
+          "justify-content": "space-between",
+        }}
+      >
+        <div>
+          <img src={logo} style={{ width: 40 }} alt="icon" /> Resilience
+        </div>
+
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            console.log(credentialResponse)
+          }}
+          onError={() => {
+            console.log("Login Failed")
+          }}
+        />
+      </header>
+
+      <Divider style={{ margin: "0px" }} />
       <div
         style={{
           display: "flex",
-          flexDirection: "column",
+          flex: 1,
+          flexDirection: "row",
+          "align-items": "top",
           "justify-content": "space-between",
-          height: "100vh",
         }}
       >
-        <header
-          style={{
-            margin: "10px",
-            display: "flex",
-            msFlexDirection: "row",
-            "justify-content": "space-between",
-          }}
-        >
-          <div>
-            <img src={logo} style={{ width: 40 }} alt="icon" /> Resilience
-          </div>
-
-          <Radio.Group
-            value={componentSize}
-            onChange={(e) => {
-              setComponentSize(e.target.value)
+        <div style={{ display: "flex", msFlexDirection: "row" }}>
+          <Menu
+            defaultSelectedKeys={["/"]}
+            mode="inline"
+            style={{ width: "30rem" }}
+            onClick={({ key }) => {
+              navigate(key)
             }}
           >
-            <Radio.Button value="small">Small</Radio.Button>
-            <Radio.Button value="large">Large</Radio.Button>
-          </Radio.Group>
-
-          <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              console.log(credentialResponse)
-            }}
-            onError={() => {
-              console.log("Login Failed")
-            }}
-          />
-        </header>
-
-        <Divider style={{ margin: "0px" }} />
-        <div
-          style={{
-            display: "flex",
-            flex: 1,
-            flexDirection: "row",
-            "align-items": "top",
-            "justify-content": "space-between",
-          }}
-        >
-          <div style={{ display: "flex", msFlexDirection: "row" }}>
-            <Menu
-              defaultSelectedKeys={["/"]}
-              mode="inline"
-              style={{ width: "30rem" }}
-              onClick={({ key }) => {
-                navigate(key)
-              }}
+            <Menu.Item key="/">Home</Menu.Item>
+            <SubMenu
+              key="roles"
+              title={<span>Roles</span>}
+              icon={<SettingOutlined />}
             >
-              <Menu.Item key="/">Home</Menu.Item>
-              <SubMenu
-                key="roles"
-                title={<span>Roles</span>}
-                icon={<SettingOutlined />}
-              >
-                <Menu.Item key="/roles">All roles</Menu.Item>
-                <Menu.Item key="/roles/add">Add role</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="executors"
-                title={<span>Executors</span>}
-                icon={<TeamOutlined />}
-              >
-                <Menu.Item key="/executors">All executors</Menu.Item>
-                <Menu.Item key="/executors/add">Add executor</Menu.Item>
-              </SubMenu>
-            </Menu>
+              <Menu.Item key="/roles">All roles</Menu.Item>
+              <Menu.Item key="/roles/add">Add role</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="executors"
+              title={<span>Executors</span>}
+              icon={<TeamOutlined />}
+            >
+              <Menu.Item key="/executors">All executors</Menu.Item>
+              <Menu.Item key="/executors/add">Add executor</Menu.Item>
+            </SubMenu>
+          </Menu>
 
-            <Routes element={1}>
-              <Route path="/role/:id" element={<Role />}></Route>
-              <Route path="/" element={<Home />}></Route>
+          <Routes element={1}>
+            <Route path="/role/:id" element={<Role />}></Route>
+            <Route path="/" element={<Home />}></Route>
 
-              <Route path="/roles" key="/roles" element={<Roles />}></Route>
-              <Route path="/roles/add" element={<AddRole />}></Route>
-              <Route
-                path="/executors"
-                key="/executors"
-                element={<Executors />}
-              ></Route>
-              <Route path="/executors/add" element={<AddExecutor />}></Route>
-              <Route
-                path="/executor/:id"
-                key="/executor/:id"
-                element={<Executor />}
-              ></Route>
-            </Routes>
-          </div>
+            <Route path="/roles" key="/roles" element={<Roles />}></Route>
+            <Route path="/roles/add" element={<AddRole />}></Route>
+            <Route
+              path="/executors"
+              key="/executors"
+              element={<Executors />}
+            ></Route>
+            <Route path="/executors/add" element={<AddExecutor />}></Route>
+            <Route
+              path="/executor/:id"
+              key="/executor/:id"
+              element={<Executor />}
+            ></Route>
+          </Routes>
         </div>
-
-        <Divider style={{ margin: "0px" }} />
-
-        <footer
-          style={{
-            textAlign: "center",
-          }}
-        >
-          Oleksii Hnatiienko
-        </footer>
       </div>
-    </ConfigProvider>
+
+      <Divider style={{ margin: "0px" }} />
+
+      <footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Oleksii Hnatiienko
+      </footer>
+    </div>
   )
 }
 
