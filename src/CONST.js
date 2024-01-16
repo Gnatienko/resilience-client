eb4f9b9e906b1d96e480fa36fcb6b115f44bf4dd
Gnatienko <gnatienko@gmail.com>
2024-01-17 01:16:58 +0200

    1

--- src/CONST.js
+++ src/CONST.js
@@ -1,3 +1,3 @@
 export const WORKING_HOURS_PER_WEEK = 40
-export const CORE_URL = "https://swanky-fossil-dawn.glitch.me"
+export const CORE_URL = "https://freckle-hot-indigo.glitch.me/"
 export const TEST_CORE_URL = "http://localhost:3005"
