f6dacce916af3bbbf13c0b3c86756444d5f21607
Gnatienko <gnatienko@gmail.com>
2024-01-17 18:22:31 +0200

    21

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -7,7 +7,7 @@ import "./SignIn.css"
 const SignIn = () => {
   const [jwtToken, setJwtToken] = useState(null)
   const [loading, setLoading] = useState(false)
-  const maxRetries = 20
+  const maxRetries = 21
   const retryDelay = 1000
   const [messageApi, contextHolder] = message.useMessage()
 
